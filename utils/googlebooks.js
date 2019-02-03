8ce9ff734a66b9277e817380626291533181b6a6
innkeeper77 <30507952+innkeeper77@users.noreply.github.com>
2019-02-03 13:05:06 -0700

    Commit 3

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -8,7 +8,18 @@ module.exports = async (phrase) => {
         }
     })
 
-    return JSON.stringify(results.data);
+    var response = {
+        data: results.data,  // results. or response.? Using results because that seems to work
+        status: results.status,
+        statusText: results.statusText,
+        headers: results.headers,
+        requestHeader: results.requestHeaders,
+    };
+
+    console.log(results.headers);
+    console.log(results.config.headers);
+
+    return JSON.stringify(response);
 }
 
 /*  console.log(results.data);
